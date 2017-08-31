c0a352ab0e4fd967a1ac4e5f7c86ce36590867e0
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-08-31 15:54:11 -0600

    Adds a p0 lexer, parser, and AST
    Added partial ast and lalrpop definition
    
    Added definition for p0 ast in ast.rs mod comment
    
    Finished test for p0::parse_name, fixed underscore parse case
    
    Changed decimal_integer to decimal_i32, added ast struct
    
    Added lalrpop def for decimal_i32
    
    Added p0::parse_decimal_i32 and tests (1 failing)
    
    There is an issue where the current parser will allow 01 to be parsed as
    0 and then 1, however I believe this will ultimately get caught in a higher
    level of the grammar, ie DecimalI32 DecimalI32 will not be allowed
    
    Added p0::parse_expression
    
    Added ast::Expression and ast::UnaryNeg, added p0::parse_expression.
    It might be worthwhile to consider eliminating the extra indirection between
    the variants of Expression and their struct representations
    
    (ie, remove all expression variant types from ast.rs and just leave
        Expression {
            Name(String),
            DecimalI32(i32),
            ...
        }
    )
    
    Added UnaryNeg, added error handling for i32 parsing
    
    Added test for p0::parse_unary_neg
    
    Removed individual ast structs, merged into Expression enum
    
    Changed lalrpop to have separate expression variant parsers
    
    Disambiguated the grammar
    
    Updated test suite after changes to parsing
    
    Added partial testing of binary expressions and precedence
    
    Needed to refactor most everything for custom lexer
    
    Added src/p0.rs to gitignore, not sure if this is a good idea
    
    Implemented Lexer::name_or_keyword
    
    Added Input to p0.lalrpop
    
    Added statements parsing, needs testing
    
    Refactored statements parsing
    
    I didn't realize I could just do <v:statement*>!
    
    Added statements parsing test
    
    Fixed decimal_i32 (previously wasn't parsing negative numbers
    
    Added testing macro
    
    Changed macro to function
    
    Added test func and macro, ran rustfmt
    
    Split int test into 4
    
    Added test for parse_decimal("01")
    
    Refactored test closures into two macros
    
    Refactored decimal_i32 parsing, fixed min_value parsing error
    
    Updated parse_01 to match currently expected behavior
    
    Added gitattributes to prevent Windows from adding CLRF on my system
    
    Added 3 more integer parsing tests
    
    rustfmt and added some tests for name parsing
    
    Added test for parsing names containing invalid chars (currently failing
    
    Lexer throws unexpected char if non-whitespace non-token char encountered
    
    Added test for parsing names containing invalid characters
    
    Added more tests for parsing names
    
    Added test for parens term parsing
    
    Check parsing input() expressions
    
    Added test for parsing print statements
    
    Added test for parsing assign statements
    
    Added another test to parse_statements
    
    Fixed warnings
    
    Modified grammar definition in ast.rs
    
    Removed old test suite (which was commented out) and rustfmt
    
    Removed changes to main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,454 @@
+extern crate lalrpop_util;
+extern crate regex;
+
+// dev-dependency for testing
+
+#[cfg(test)]
+extern crate rand;
+
+pub mod p0;
+pub mod lexer;
+pub mod ast;
+
 #[cfg(test)]
 mod test {
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use lexer;
+    use p0;
+    use ast::*;
+    use std::fmt::Debug;
+
+    fn test<'input, F, E, T, R>(parse: F, s: &'input str, test: T) -> R
+    where
+        F: FnOnce(&'input str, lexer::Lexer<'input>) -> E,
+        E: Debug + PartialEq,
+        T: FnOnce(E) -> R,
+    {
+        let lexer = lexer::Lexer::new(s);
+        let parsed = parse(s, lexer);
+        test(parsed)
+    }
+
+    macro_rules! test {
+        (
+            parse: $func:expr,
+            input: $input:expr,
+            test: $test:expr
+        ) => ({
+            test($func, $input, $test)
+        })
+    }
+
+    macro_rules! parsed_is_err {
+        () => ({
+            |parsed| {
+                assert!(parsed.is_err(), "parsed is not err: {:#?}", parsed);
+            }
+        })
+    }
+
+    macro_rules! parsed_equals {
+        ($expected:expr) => ({
+            |parsed| {
+                let parsed = parsed.unwrap();
+                let expected = $expected;
+                assert_eq!(parsed, expected, "parsed: {:#?}, expected: {:#?}", parsed, expected);
+            }
+        })
+    }
+
+    macro_rules! expected_ne {
+        ($parsed:expr) => ({
+            let parsed = $parsed;
+            assert!(false, "parsed {:#?}, expected NOT equal", parsed);
+        })
+    }
+
+    #[test]
+    fn parse_assign_statement() {
+        test! {
+            parse: p0::parse_statement,
+            input: "x = 1",
+            test: parsed_equals!(
+                Statement::Assign(
+                    Name::new(b"x").unwrap(),
+                    Expression::DecimalI32(DecimalI32(1))
+                )
+            )
+        }
+
+        test! {
+            parse: p0::parse_statement,
+            input: "x = (-1 + x + (-y + 4))",
+            test: parsed_equals!(
+                Statement::Assign(
+                    Name::new(b"x").unwrap(),
+                    Expression::Add(
+                        // - 1 + x
+                        Expression::Add(
+                            Expression::DecimalI32(DecimalI32(-1)).into(),
+                            Expression::Name(Name::new(b"x").unwrap()).into()
+                        ).into(),
+                        // -y + 4
+                        Expression::Add(
+                            Expression::UnaryNeg(Expression::Name(Name::new(b"y").unwrap()).into()).into(),
+                            Expression::DecimalI32(DecimalI32(4)).into()
+                        ).into()
+                    )
+                )
+            )
+        }
+    }
+
+    #[test]
+    fn parse_print_statement() {
+        test! {
+            parse: p0::parse_statement,
+            input: "print 1 + x",
+            test: parsed_equals!(
+                Statement::Print(
+                    Expression::Add(
+                        Expression::DecimalI32(DecimalI32(1)).into(),
+                        Expression::Name(Name::new(b"x").unwrap()).into()
+                    )
+                )
+            )
+        }
+        test! {
+            parse: p0::parse_statement,
+            input: "print1",
+            test: |parsed| {
+                match parsed {
+                    Ok(Statement::Print(_)) => expected_ne!(parsed),
+                    _ => {}
+                }
+            }
+        }
+        test! {
+            parse: p0::parse_statement,
+            input: "printx+2",
+            test: |parsed| {
+                match parsed {
+                    Ok(Statement::Print(_)) => expected_ne!(parsed),
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn parse_input_call() {
+        test! {
+            parse: p0::parse_term,
+            input: "input()",
+            test: parsed_equals!(Expression::Input(Input))
+        }
+    }
+
+    #[test]
+    fn parse_parens_term() {
+        test! {
+            parse: p0::parse_term,
+            input: "(rust_python)",
+            test: parsed_equals!(Expression::Name(Name::new(b"rust_python").unwrap()))
+        }
+        test! {
+            parse: p0::parse_term,
+            input: "( (( rust_python + 222 )) )",
+            test: parsed_equals!(
+                Expression::Add(
+                    Expression::Name(Name::new(b"rust_python").unwrap()).into(),
+                    Expression::DecimalI32(DecimalI32(222)).into()
+                )
+            )
+        }
+        test! {
+            parse: p0::parse_term,
+            input: "( 1829102 + 291 )",
+            test: parsed_equals!(
+                Expression::Add(
+                    Expression::DecimalI32(DecimalI32(1829102)).into(),
+                    Expression::DecimalI32(DecimalI32(291)).into()
+                )
+            )
+        }
+        test! {
+            parse: p0::parse_term,
+            input: "-(_ + 2)",
+            test: parsed_equals!(
+                Expression::UnaryNeg(
+                    Expression::Add(
+                        Expression::Name(Name::new(b"_").unwrap()).into(),
+                        Expression::DecimalI32(DecimalI32(2)).into()
+                    ).into()
+                )
+            )
+        }
+
+    }
+
+    #[test]
+    fn parse_name_with_uppercase_letters() {
+        let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for c in uppercase.chars() {
+            let name = format!("rust{c}python", c = c);
+            test! {
+                parse: p0::parse_name,
+                input: name.as_str(),
+                test: parsed_equals!(Name::new(name.as_bytes()).unwrap())
+            }
+        }
+    }
+
+    #[test]
+    fn parse_name_with_lowercase_letters() {
+        let lowercase = "abcdefghijklmnopqrstuvwxyz";
+        for c in lowercase.chars() {
+            let name = format!("rust{c}python", c = c);
+            test! {
+                parse: p0::parse_name,
+                input: name.as_str(),
+                test: parsed_equals!(Name::new(name.as_bytes()).unwrap())
+            }
+        }
+    }
+
+    #[test]
+    fn parse_name_with_numbers() {
+        for n in 0..10 {
+            let name = format!("rust{n}python", n = n);
+            test! {
+                parse: p0::parse_name,
+                input: name.as_str(),
+                test: parsed_equals!(Name::new(name.as_bytes()).unwrap())
+            }
+        }
+    }
+
+    #[test]
+    fn parse_name_with_all_valid_char_types() {
+        let names = &[
+            "_a9",
+            "b_0",
+            "c0_",
+            "rust_python2",
+            "_rust___python___292921901",
+            "r_jdjf929291lakd_9929292",
+        ];
+
+        for name in names {
+            test! {
+                parse: p0::parse_name,
+                input: name,
+                test: parsed_equals!(Name::new(name.as_bytes()).unwrap())
+            }
+        }
+    }
+
+    #[test]
+    fn parse_name_containing_invalid_characters() {
+        let invalid = "!@#$%^&*()-+=[]{}|\\\"':;?><,./";
+        for c in invalid.chars() {
+            let name = format!("rust{c}python", c = c);
+            test! {
+                parse: p0::parse_expression,
+                input: name.as_str(),
+                test: |parsed| {
+                    match parsed {
+                        Ok(Expression::Name(_)) => {
+                            expected_ne!(parsed);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn parse_name_starting_with_number() {
+        for n in 0..10 {
+            let name = format!("{n}rust_python", n = n);
+            test! {
+                parse: p0::parse_name,
+                input: name.as_str(),
+                test: parsed_is_err!()
+            }
+        }
+    }
+
+    #[test]
+    fn parse_underscore_first_letter() {
+        test! {
+            parse: p0::parse_name,
+            input: "_rust_python",
+            test: parsed_equals!(Name::new(b"_rust_python").unwrap())
+        }
+    }
+
+    #[test]
+    fn parse_underscore_name() {
+        test! {
+            parse: p0::parse_name,
+            input: "_",
+            test: parsed_equals!(Name::new(b"_").unwrap())
+        }
+    }
+
+    #[test]
+    fn parse_negative_number() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: "-71",
+            test: parsed_equals!(DecimalI32(-71))
+        }
+    }
+
+    #[test]
+    fn parse_positive_number() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: "906",
+            test: parsed_equals!(DecimalI32(906))
+        }
+    }
+
+    #[test]
+    fn parse_number_zero() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: "0",
+            test: parsed_equals!(DecimalI32(0))
+        }
+
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: "-0",
+            test: parsed_equals!(DecimalI32(0))
+        }
+    }
+
+    #[test]
+    fn parse_01() {
+        // TODO: This might not be the right
+        // behavior to expect
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: "01",
+            test: parsed_equals!(DecimalI32(0))
+        }
+    }
+
+    #[test]
+    fn parse_i32_min_value() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: format!("{}", i32::min_value()).as_str(),
+            test: parsed_equals!(DecimalI32(i32::min_value()))
+        }
+    }
+
+    #[test]
+    fn parse_i32_max_value() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: format!("{}", i32::max_value()).as_str(),
+            test: parsed_equals!(DecimalI32(i32::max_value()))
+        }
+    }
+
+    #[test]
+    fn parse_i32_max_value_plus1() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: format!("{}", i32::max_value() as i64 + 1).as_str(),
+            test: parsed_is_err!()
+        }
+    }
+
+    #[test]
+    fn parse_i32_min_value_minus1() {
+        test! {
+            parse: p0::parse_decimal_i32,
+            input: format!("{}", i32::min_value() as i64 - 1).as_str(),
+            test: parsed_is_err!()
+        }
+    }
+
+    #[test]
+    fn parse_statement() {
+        let statement = "1 + 2";
+        let lexer = lexer::Lexer::new(statement);
+        assert_eq!(
+            p0::parse_statement(statement, lexer),
+            Ok(Statement::Expression(Expression::Add(
+                Expression::DecimalI32(DecimalI32(1)).into(),
+                Expression::DecimalI32(DecimalI32(2)).into(),
+            )))
+        );
+    }
+
+    #[test]
+    fn parse_statements() {
+        let statements = "\n\nprint 1 + 2\n\n3 + 4\n\n\n";
+        let lexer = lexer::Lexer::new(statements);
+        assert_eq!(
+            p0::parse_statements(statements, lexer),
+            Ok(vec![
+                Statement::Newline,
+                Statement::Newline,
+                Statement::Print(Expression::Add(
+                    Expression::DecimalI32(DecimalI32(1)).into(),
+                    Expression::DecimalI32(DecimalI32(2)).into(),
+                )),
+                Statement::Newline,
+                Statement::Newline,
+                Statement::Expression(Expression::Add(
+                    Expression::DecimalI32(DecimalI32(3)).into(),
+                    Expression::DecimalI32(DecimalI32(4)).into(),
+                )),
+                Statement::Newline,
+                Statement::Newline,
+                Statement::Newline,
+            ])
+        );
+
+        test! {
+            parse: p0::parse_statements,
+            input: "\n\n1 + 2 \n\n \nx +1\nprint 1 + 2 + 3 + -4",
+            test: parsed_equals!(
+                vec![
+                    Statement::Newline,
+                    Statement::Newline,
+                    Statement::Expression(
+                        Expression::Add(
+                            Expression::DecimalI32(DecimalI32(1)).into(),
+                            Expression::DecimalI32(DecimalI32(2)).into()
+                        ),
+                    ),
+                    Statement::Newline,
+                    Statement::Newline,
+                    Statement::Newline,
+                    Statement::Expression(
+                        Expression::Add(
+                            Expression::Name(Name::new(b"x").unwrap()).into(),
+                            Expression::DecimalI32(DecimalI32(1)).into()
+                        )
+                    ),
+                    Statement::Newline,
+                    Statement::Print(
+                        Expression::Add(
+                            Expression::Add(
+                                Expression::Add(
+                                    Expression::DecimalI32(DecimalI32(1)).into(),
+                                    Expression::DecimalI32(DecimalI32(2)).into(),
+                                ).into(),
+                                Expression::DecimalI32(DecimalI32(3)).into()
+                            ).into(),
+                            Expression::DecimalI32(DecimalI32(-4)).into()
+                        )
+                    )
+                ]
+            )
+        }
     }
 }
