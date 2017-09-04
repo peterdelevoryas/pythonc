4ad32c5a068e0fdb5d1a27591a90a806bbf98b9d
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-03 20:36:55 -0600

    Further iteration on design of ir

--- src/ir.rs
+++ src/ir.rs
@@ -28,45 +28,77 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Program {
-    statements: Vec<Statement>,
+    stmts: Vec<Stmt>,
 }
 
-/// Def(index) -> index of Def in stack
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum Operand {
-    Constant(i32),
-    Def(usize),
+pub struct Tmp {
+    index: usize
+}
+
+/// Tmp(index) -> index of Tmp in stack
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum Val {
+    Int(i32),
+    Tmp(Tmp),
 }
 
 ///
-///     tmp8 = tmp7     ; Copy(Def)
+///     tmp8 = tmp7     ; Copy(Tmp)
 ///     tmp0 = 1        ; Copy(Constant)
 ///     tmp1 = -1       ; UnaryNeg(Constant)
-///     tmp2 = -tmp1    ; UnaryNeg(Def)
+///     tmp2 = -tmp1    ; UnaryNeg(Tmp)
 ///     tmp3 = 1 + 2    ; Add(Constant, Constant)
-///     tmp4 = 1 + tmp3 ; Add(Constant, Def)
-///     tmp5 = tmp4 + 1     ; Add(Def, Constant)
-///     tmp6 = tmp4 + tmp5  ; Add(Def, Def)
+///     tmp4 = 1 + tmp3 ; Add(Constant, Tmp)
+///     tmp5 = tmp4 + 1     ; Add(Tmp, Constant)
+///     tmp6 = tmp4 + tmp5  ; Add(Tmp, Tmp)
 ///     tmp7 = input()  ; Input
 ///
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Def {
-    Copy(Operand),
-    UnaryNeg(Operand),
-    Add(Operand, Operand),
+pub enum Expr {
+    Copy(Val),
+    UnaryNeg(Val),
+    Add(Val, Val),
     Input,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub enum Statement {
-    Print(Operand),
-    Def(Def),
+pub enum Stmt {
+    Print(Val),
+    Def(Tmp, Expr),
 }
 
 #[derive(Debug)]
 pub struct Builder {
-    stack: Vec<Statement>,
-    names: HashMap<ast::Name, Operand>,
+    stack: Vec<Stmt>,
+    names: HashMap<ast::Name, Tmp>,
+    tmp_allocator: TmpAllocator,
+}
+
+#[derive(Debug)]
+pub struct TmpAllocator {
+    next: usize,
+}
+
+impl TmpAllocator {
+    pub fn new() -> TmpAllocator {
+        TmpAllocator {
+            next: 0
+        }
+    }
+
+    pub fn alloc(&mut self) -> Option<Tmp> {
+        let tmp = Tmp {
+            index: self.next
+        };
+        self.next = match self.next.checked_add(1) {
+            Some(sum) => sum,
+            None => return None,
+        };
+        Some(tmp)
+    }
+
+    
 }
 
 impl Builder {
@@ -74,34 +106,46 @@ impl Builder {
         Builder {
             stack: vec![],
             names: HashMap::new(),
+            tmp_allocator: TmpAllocator::new(),
         }
     }
 
-    pub fn expr_to_operand(&mut self, expr: &ast::Expression) -> Operand {
-        match *expr {
+    pub fn flatten_expression(&mut self, expression: &ast::Expression) -> Expr {
+        match *expression {
             ast::Expression::DecimalI32(ast::DecimalI32(i)) => {
-                Operand::Constant(i)
+                Expr::Copy(Val::Int(i))
             }
             ast::Expression::Name(ref name) => {
-                *self.names.get(name).expect("reference to undefined name")
+                let tmp = match self.names.get(name) {
+                    Some(&tmp) => tmp,
+                    None => panic!("reference to undefined name {:?}", name),
+                };
+                Expr::Copy(Val::Tmp(tmp))
             }
             _ => unimplemented!()
         }
     }
 
-    pub fn handle_statement(&mut self, stmt: &ast::Statement) {
-        match *stmt {
-            ast::Statement::Print(ref expr) => {
+    pub fn flatten_statement(&mut self, statement: &ast::Statement) {
+        match *statement {
+            ast::Statement::Print(ref expression) => {
                 
             }
-            ast::Statement::Assign(ref name, ref expr) => {
-
+            ast::Statement::Assign(ref name, ref expression) => {
+                let tmp = self.tmp_allocator.alloc().expect("tmp allocator oom");
+                let expr = self.flatten_expression(expression);
+                self.push(Stmt::Def(tmp, expr));
+                self.names.insert(name.clone(), tmp);
             }
-            ast::Statement::Expression(ref expr) => {
+            ast::Statement::Expression(ref expression) => {
 
             }
             ast::Statement::Newline => {},
         }
+    }
+
+    pub fn push(&mut self, s: Stmt) {
+        self.stack.push(s);
     }
 }
 
