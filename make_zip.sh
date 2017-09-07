eea0fb61657f019cf67f9673bd9bcf5a2e78cb2e
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-07 13:22:04 -0600

    Prefixed rustup and cargo commands with full paths

--- make_zip.sh
+++ make_zip.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 curl https://sh.rustup.rs -sSf | sh
-rustup target add i686-unknown-linux-gnu
-cargo build --target=i686-unknown-linux-gnu
+$HOME/.cargo/bin/rustup target add i686-unknown-linux-gnu
+$HOME/.cargo/bin/cargo build --target=i686-unknown-linux-gnu
 cp ./target/i686-unknown-linux-gnu/debug/rust-python .
 file ./rust-python
 zip -r cog.zip build.rs Cargo.* Makefile make_zip.sh pyyc README.md runtime rust-python src
