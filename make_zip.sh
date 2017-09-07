0cd4bff4b44baac6cc58df4622f83c345a6cf4da
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-07 13:18:32 -0600

    Reordered make_zip.sh file and cp

--- make_zip.sh
+++ make_zip.sh
@@ -2,7 +2,6 @@
 
 rustup target add i686-unknown-linux-gnu
 cargo build --target=i686-unknown-linux-gnu
-echo "File: "
-file ./target/i686-unknown-linux-gnu/debug/rust-python
 cp ./target/i686-unknown-linux-gnu/debug/rust-python .
+file ./rust-python
 zip -r cog.zip build.rs Cargo.* Makefile make_zip.sh pyyc README.md runtime rust-python src
