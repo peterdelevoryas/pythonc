677f5d26570fbfe77f4be2dfb9e8008ff16d10e4
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-06 22:39:54 -0600

    Refactored reg! and created register_set! and register!

--- src/x86/reg.rs
+++ src/x86/reg.rs
@@ -12,33 +12,82 @@ mod sealed {
     pub trait Sealed {}
 }
 
-macro_rules! reg {
+macro_rules! register_set {
     (
-        $reg32:ident: $Reg32:ident {
+        $Set:ident {
+        $(
+            $reg32:ident: $Reg32:ident {
             $(
                 $reg16:ident: $Reg16:ident {
-                    $(
-                        $reg8: ident: $Reg8:ident
-                    ),*
+                $(
+                    $reg8: ident: $Reg8:ident
+                ),*
                 }
             ),*
+            }
+        ),*
+        }
+    ) => {
+        $(
+        register! {
+            $Reg32 {
+            $(
+                $reg16: $Reg16 {
+                $(
+                    $reg8: $Reg8
+                ),*
+                }
+            ),*
+            }
+        }
+        )*
+
+        #[derive(Debug)]
+        pub struct $Set {
+        $(
+            $reg32: $Reg32
+        ),*
+        }
+
+        impl $Set {
+            pub fn new() -> $Set {
+                $Set {
+                $(
+                    $reg32: $Reg32::new()
+                ),*
+                }
+            }
+        }
+    }
+}
+
+macro_rules! register {
+    (
+        $Reg32:ident {
+        $(
+            $reg16:ident: $Reg16:ident {
+            $(
+                $reg8: ident: $Reg8:ident
+            ),*
+            }
+        ),*
         }
     ) => {
         #[derive(Debug, PartialEq, Eq, Hash)]
         pub struct $Reg32 {
             private: (),
-            $(
-                pub $reg16: $Reg16,
-            ),*
+        $(
+            pub $reg16: $Reg16,
+        ),*
         }
 
         impl $Reg32 {
             fn new() -> $Reg32 {
                 $Reg32 {
                     private: (),
-                    $(
-                        $reg16: $Reg16::new()
-                    ),*
+                $(
+                    $reg16: $Reg16::new()
+                ),*
                 }
             }
         }
@@ -50,148 +99,87 @@ macro_rules! reg {
         }
 
         $(
-            #[derive(Debug, PartialEq, Eq, Hash)]
-            pub struct $Reg16 {
-                private: (),
-                $(
-                    pub $reg8: $Reg8
-                ),*
-            }
+        #[derive(Debug, PartialEq, Eq, Hash)]
+        pub struct $Reg16 {
+            private: (),
+        $(
+            pub $reg8: $Reg8
+        ),*
+        }
 
-            impl $Reg16 {
-                fn new() -> $Reg16 {
-                    $Reg16 {
-                        private: (),
-                        $(
-                            $reg8: $Reg8::new()
-                        ),*
-                    }
+        impl $Reg16 {
+            fn new() -> $Reg16 {
+                $Reg16 {
+                    private: (),
+                $(
+                    $reg8: $Reg8::new()
+                ),*
                 }
             }
+        }
 
-            impl Sealed for $Reg16 {}
-            impl Reg16 for $Reg16 {}
+        impl Sealed for $Reg16 {}
+        impl Reg16 for $Reg16 {}
         )*
 
         $(
-            $(
-                #[derive(Debug, PartialEq, Eq, Hash)]
-                pub struct $Reg8 {
-                    private: (),
-                }
-
-                impl $Reg8 {
-                    fn new() -> $Reg8 {
-                        $Reg8 { private: () }
-                    }
-                }
-
-                impl Sealed for $Reg8 {}
-                impl Reg8 for $Reg8 {}
-            )*
-        )*
-    };
-
-    (
-        $reg32:ident: $Reg32:ident {
-            $(
-                $reg16:ident: $Reg16:ident
-            ),*
+        $(
+        #[derive(Debug, PartialEq, Eq, Hash)]
+        pub struct $Reg8 {
+            private: (),
         }
-    ) => {
-        reg! {
-            $reg32: $Reg32 {
-                $(
-                    $reg16: $Reg16 {}
-                ),*
+
+        impl $Reg8 {
+            fn new() -> $Reg8 {
+                $Reg8 { private: () }
             }
         }
+
+        impl Sealed for $Reg8 {}
+        impl Reg8 for $Reg8 {}
+        )*
+        )*
+
     }
 }
 
-reg! {
-    eax: EAX {
-        ax: AX {
-            ah: AH,
-            al: AL
-        }
-    }
-}
-
-reg! {
-    eax: ECX {
-        cx: CX {
-            ch: CH,
-            cl: CL
-        }
-    }
-}
-
-reg! {
-    edx: EDX {
-        dx: DX {
-            dh: DH,
-            dl: DL
-        }
-    }
-}
-
-reg! {
-    ebx: EBX {
-        bx: BX {
-            bh: BH,
-            bl: BL
-        }
-    }
-}
-
-reg! {
-    esp: ESP {
-        sp: SP
-    }
-}
-
-reg! {
-    ebp: EBP {
-        bp: BP
-    }
-}
-
-reg! {
-    esi: ESI {
-        si: SI
-    }
-}
-
-reg! {
-    edi: EDI {
-        di: DI
-    }
-}
-
-#[derive(Debug)]
-pub struct Set {
-    pub eax: EAX,
-    pub ecx: ECX,
-    pub edx: EDX,
-    pub ebx: EBX,
-    pub esp: ESP,
-    pub ebp: EBP,
-    pub esi: ESI,
-    pub edi: EDI,
-}
-
-impl Set {
-    pub fn new() -> Set {
-        Set {
-            eax: EAX::new(),
-            ecx: ECX::new(),
-            edx: EDX::new(),
-            ebx: EBX::new(),
-            esp: ESP::new(),
-            ebp: EBP::new(),
-            esi: ESI::new(),
-            edi: EDI::new(),
+register_set! {
+    X86 {
+        eax: EAX {
+            ax: AX {
+                ah: AH,
+                al: AL
+            }
+        },
+        ecx: ECX {
+            cx: CX {
+                ch: CH,
+                cl: CL
+            }
+        },
+        edx: EDX {
+            dx: DX {
+                dh: DH,
+                dl: DL
+            }
+        },
+        ebx: EBX {
+            bx: BX {
+                bh: BH,
+                bl: BL
+            }
+        },
+        esp: ESP {
+            sp: SP {}
+        },
+        ebp: EBP {
+            bp: BP {}
+        },
+        esi: ESI {
+            si: SI {}
+        },
+        edi: EDI {
+            di: DI {}
         }
     }
 }
