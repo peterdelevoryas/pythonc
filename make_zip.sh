a909ab66950ebcfe0a54c1fa5a025f24440d616e
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-08 15:20:15 -0600

    Modified make_zip.sh to include Makefile again

--- make_zip.sh
+++ make_zip.sh
@@ -5,4 +5,4 @@ $HOME/.cargo/bin/rustup target add i686-unknown-linux-gnu
 $HOME/.cargo/bin/cargo build --target=i686-unknown-linux-gnu
 cp ./target/i686-unknown-linux-gnu/debug/rust-python .
 file ./rust-python
-zip -r cog.zip build.rs Cargo.* make_zip.sh pyyc README.md runtime rust-python src
+zip -r cog.zip build.rs Cargo.* Makefile make_zip.sh pyyc README.md runtime rust-python src
