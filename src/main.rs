b86097c8f5a8693159db1187dfc1a93c379231dd
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-11 00:48:36 -0600

    Implementing docopt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
 extern crate pythonc;
 
-fn main() {
+use docopt::Docopt;
 
+const USAGE: &str = "
+pythonc.
+
+Usage:
+    pythonc --emit-asm <file>
+    pythonc (-h | --help)
+    pythonc --version
+
+Options:
+    -h --help   Show this message.
+    --version   Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_emit_asm: bool,
+    flag_version: bool,
+    arg_file: String,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+    if args.flag_version {
+        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    }
+    println!("{:?}", args);
 }
