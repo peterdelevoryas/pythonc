4948fa24547bd2a5d9b4d4de2b6cbb301a32a359
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-10 20:17:22 -0600

    Added ast Parser methods

--- src/ast/parse/mod.rs
+++ src/ast/parse/mod.rs
@@ -26,4 +26,32 @@ impl<S> Parser<S>
     pub fn new(stream: S) -> Parser<S> {
         Parser { stream }
     }
+
+}
+    
+// TODO There has to be a better way to do this
+// than just making a macro around it, or maybe
+// it's just unnecessary to have this Parser<S>
+// struct.
+macro_rules! impl_parser_methods {
+    ($($parse_fn:ident -> $data:ty;)+) => {
+        impl<S> Parser<S>
+            where S: Iterator<Item=tok::Spanned<tok::Tok>>
+        {
+            $(
+                pub fn $parse_fn(&mut self) -> Result<$data> {
+                    p0::$parse_fn(&mut self.stream)
+                }
+            )+
+        }
+    }
+}
+
+impl_parser_methods! {
+    parse_decimal_i32 -> DecimalI32;
+    parse_name -> Name;
+    parse_expression -> Expression;
+    parse_statement -> Statement;
+    parse_module -> Module;
+    parse_program -> Program;
 }
