fff5ba309cb11134d58df9f35d4cdb7fd72403b9
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-19 05:20:04 -0600

    Added fmt::Display impl for Register32

--- crates/trans/src/register.rs
+++ crates/trans/src/register.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Register32 {
     EAX,
@@ -8,6 +10,23 @@ pub enum Register32 {
     EDI,
     ESP,
     EBP,
+}
+
+impl fmt::Display for Register32 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Register32::*;
+        let s = match *self {
+            EAX => "eax",
+            EBX => "ebx",
+            ECX => "ecx",
+            EDX => "edx",
+            ESI => "esi",
+            EDI => "edi",
+            ESP => "esp",
+            EBP => "ebp",
+        };
+        write!(f, "%{}", s)
+    }
 }
 
 #[cfg(test)]
