47ec61639e6d097b20a7864ad76b79bad8f5fb2d
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-28 22:46:59 -0600

    Added enumerate_backwards test

--- crates/liveness/src/lib.rs
+++ crates/liveness/src/lib.rs
@@ -3,19 +3,43 @@ extern crate python_ir as ir;
 
 use std::collections::HashSet;
 
-pub fn liveness<'ir>(ir: &'ir ir::Program) -> impl 'ir + Iterator<Item=HashSet<ir::Tmp>> {
-    ir.stmts
-        .iter()
-        .rev()
-        .map(|_| {
-            HashSet::new()
-        })
+pub struct Liveness {
+    // see course notes, k is statement index
+    k: usize,
+    live_after_k: HashSet<ir::Tmp>,
+}
+
+pub fn compute(ir: &ir::Program) -> Vec<Liveness> {
+    let mut stack = Vec::new();
+
+    for (k, stmt) in ir.stmts.iter().enumerate().rev() {
+        let live = Liveness {
+            k,
+            live_after_k: HashSet::new(),
+        };
+        stack.push(live);
+    }
+
+    stack
 }
 
 #[cfg(test)]
 mod tests {
+    use super::compute;
+    use ir;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn enumerate_backwards() {
+        let ir = ir::Program {
+            stmts: vec![
+                ir::Stmt::Print(ir::Val::Int(1)),
+                ir::Stmt::Print(ir::Val::Int(2)),
+                ir::Stmt::Print(ir::Val::Int(3)),
+            ]
+        };
+        let liveness = compute(&ir);
+        for (i, l) in liveness.iter().enumerate() {
+            assert_eq!(l.k, liveness.len() - i - 1);
+        }
     }
 }
