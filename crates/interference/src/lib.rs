9d709c2a5e69377f6921e48837a02a56762e4f37
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-29 02:22:33 -0600

    Added interference crate

---
+++ crates/interference/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate liveness;
+extern crate python_ir as ir;
+extern crate petgraph;
+
+pub fn build_graph(
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
