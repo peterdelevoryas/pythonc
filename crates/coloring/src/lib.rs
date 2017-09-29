eaab9f814ff2a99c6255d3ab6b52f957818b4e0f
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-29 06:48:40 -0600

    Adding coloring

---
+++ crates/coloring/src/lib.rs
@@ -0,0 +1,16 @@
+extern crate python_vm as vm;
+extern crate python_ir as ir;
+extern crate liveness;
+extern crate interference;
+
+pub struct Builder {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
