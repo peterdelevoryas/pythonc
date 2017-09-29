dad4be5b62f728d123f054875ff52013af4831eb
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-29 04:37:11 -0600

    Added Graph type and build_graph function

--- crates/interference/src/lib.rs
+++ crates/interference/src/lib.rs
@@ -1,9 +1,19 @@
 extern crate liveness;
 extern crate python_ir as ir;
+extern crate python_vm as vm;
+extern crate python_trans as trans;
 extern crate petgraph;
 
-pub fn build_graph(
+pub enum Node {
+    Tmp(ir::Tmp),
+    Register(trans::Register),
+}
 
+pub type Graph = petgraph::Graph<Node, (), petgraph::Undirected>;
+
+pub fn build_graph(vm: &vm::Program) -> Graph {
+    unimplemented!()
+}
 
 #[cfg(test)]
 mod tests {
