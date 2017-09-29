8c59b2ee1a4ba2745bde3257f2892fe58f92aff9
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-28 22:24:50 -0600

    Added nightly feature use in liveness!

--- crates/liveness/src/lib.rs
+++ crates/liveness/src/lib.rs
@@ -1,3 +1,17 @@
+#![feature(conservative_impl_trait)]
+extern crate python_ir as ir;
+
+use std::collections::HashSet;
+
+pub fn liveness<'ir>(ir: &'ir ir::Program) -> impl 'ir + Iterator<Item=HashSet<ir::Tmp>> {
+    ir.stmts
+        .iter()
+        .rev()
+        .map(|_| {
+            HashSet::new()
+        })
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
