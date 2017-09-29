c7648480ef86a3df6917da73607e592187849388
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-09-29 04:48:58 -0600

    Added Builder to interference

--- crates/interference/src/lib.rs
+++ crates/interference/src/lib.rs
@@ -4,15 +4,49 @@ extern crate python_vm as vm;
 extern crate python_trans as trans;
 extern crate petgraph;
 
+use std::collections::HashMap;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Node {
     Tmp(ir::Tmp),
     Register(trans::Register),
 }
 
 pub type Graph = petgraph::Graph<Node, (), petgraph::Undirected>;
+pub type NodeIndex = petgraph::graph::NodeIndex;
 
-pub fn build_graph(vm: &vm::Program) -> Graph {
-    unimplemented!()
+pub struct Builder {
+    graph: Graph,
+    tmps: HashMap<Node, NodeIndex>,
+}
+
+impl Builder {
+    pub fn build_graph(vm: &vm::Program) -> Graph {
+        let mut builder = Builder {
+            graph: Graph::new_undirected(),
+            tmps: HashMap::new(),
+        };
+        
+        builder.create_vertices(vm);
+
+        unimplemented!()
+    }
+
+    fn create_vertices(&mut self, vm: &vm::Program) {
+        use vm::Instr::*;
+        // create tmp nodes and register nodes
+        for instr in &vm.stack {
+            match *instr {
+                Mov(val, tmp) => {
+
+                }
+                Neg(tmp) => {}
+                Add(val, tmp) => {}
+                Push(val) => {}
+                Call(ref label) => {}
+            }
+        }
+    }
 }
 
 #[cfg(test)]
