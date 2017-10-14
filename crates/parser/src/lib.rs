bbb84fad057b060764f9b0f57b04b095c59d318c
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-10-14 08:07:48 -0600

    Added more node parsing

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -2,46 +2,81 @@ extern crate python_ast as ast;
 #[macro_use]
 extern crate nom;
 
-use nom::IResult::Done;
+use nom::IResult::*;
 
 use std::process::Command;
 use std::str;
 use std::fmt;
 
-#[derive(Debug)]
-struct Node<'s> {
-    name: &'s str,
-    args: Vec<Value<'s>>,
-}
+type BoxNode<'a> = Box<Node<'a>>;
 
 #[derive(Debug)]
-enum Value<'s> {
-    None,
-    Node(Node<'s>),
+enum Node<'a> {
+    // Module(doc, node)
+    Module(&'a str, BoxNode<'a>),
+    Stmt(Vec<Node<'a>>),
+    Const(&'a str),
+    Discard(BoxNode<'a>),
 }
 
-named!(node<Node>,
-    do_parse!(
-        name: node_name >> 
-        args: delimited!(tag!("("), arg_list, tag!(")")) >>
-        (Node {
-            name: to_str(name),
-            args: args,
-        })
+named!(
+    node<Node>,
+    alt!(
+        map!(module, |(doc, node)| Node::Module(to_str(doc), Box::new(node)))   |
+        map!(stmt, |nodes| Node::Stmt(nodes))                                   |
+        map!(discard, |node| Node::Discard(Box::new(node)))                     |
+        map!(constant, |val| Node::Const(to_str(val)))
     )
 );
 
+named!(
+    module<&[u8], (&[u8], Node)>,
+    do_parse!(
+        tag!("Module") >>
+        doc: delimited!(tag!("("), is_not!(","), tag!(",")) >>
+        node: ws!(node) >>
+        tag!(")") >>
+        ((doc, node))
+    )
+);
+
+named!(
+    stmt<Vec<Node>>,
+    do_parse!(
+        tag!("Stmt") >>
+        nodes: delimited!(tag!("("), node_list, tag!(")")) >>
+        (nodes)
+    )
+);
+
+named!(
+    discard<Node>,
+    do_parse!(
+        tag!("Discard") >>
+        node: delimited!(tag!("("), node, tag!(")")) >>
+        (node)
+    )
+);
+
+named!(node_list<Vec<Node>>, delimited!(tag!("["), separated_list_complete!(tag!(","), ws!(node)), tag!("]")));
+
+named!(
+    constant<&[u8]>,
+    do_parse!(
+        tag!("Const") >>
+        v: delimited!(tag!("("), is_not!(")"), tag!(")")) >>
+        (v)
+    )
+);
+
+/*
 named!(arg_list<Vec<Value>>, separated_list_complete!(tag!(","), ws!(arg)));
 named!(arg<Value>, alt!(literal | map!(node, Value::Node)));
-named!(node_name, alt!(tag!("Module")   |
-                       tag!("Stmt")     |
-                       tag!("Discard")  |
-                       tag!("Const")    |
-                       tag!("UnarySub") |
-                       tag!("Printnl")));
-
 named!(literal<Value>, alt!(map!(none, |_| Value::None)));
 named!(none, tag!("None"));
+
+named!(const, 
+*/
 
 fn to_str(b: &[u8]) -> &str {
     str::from_utf8(b).unwrap()
@@ -50,9 +85,14 @@ fn to_str(b: &[u8]) -> &str {
 // Takes the repr(python.compiler.parse(source)) as input
 pub fn parse_program(s: &[u8]) -> Result<ast::Program, String> {
     //println!("received: {}", str::from_utf8(s).unwrap());
-    let parsed = node(b"Module(None, Module(None))").unwrap();
-    println!("parsed: {:#?}", parsed.1);
-    println!("remaining: {:?}", to_str(parsed.0));
+    match node(s) {
+        Done(remaining, parsed) => {
+            println!("remaining: {}", str::from_utf8(remaining).unwrap());
+            println!("parsed: {:#?}", parsed);
+        }
+        Error(e) => panic!("Error: {}", e),
+        Incomplete(s) => panic!("Incomplete: {:?}", s),
+    }
 
     unimplemented!()
 }
