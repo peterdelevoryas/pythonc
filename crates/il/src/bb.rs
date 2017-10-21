075289cbe5de5f79ee8d6eb588acecf7170e580c
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-10-21 14:03:34 -0600

    Added constructor for func::Builder

--- crates/il/src/bb.rs
+++ crates/il/src/bb.rs
@@ -8,3 +8,15 @@ pub struct Data {
     defs: Vec<(Val, Inst)>,
     term: Term,
 }
+
+// A basic block that has not finished being created yet
+// Basically equivalent to Data but without the Term'inator
+pub struct Partial {
+    defs: Vec<(Val, Inst)>,
+}
+
+impl Partial {
+    pub fn new() -> Partial {
+        Partial { defs: vec![] }
+    }
+}
--- crates/il/src/func.rs
+++ crates/il/src/func.rs
@@ -1,8 +1,33 @@
 use bb::{self, BasicBlock};
 use val::{self, Val};
 use slab::Slab;
+use std::collections::HashMap;
 
 pub struct Func {
     bbs: Slab<BasicBlock, bb::Data>,
     vals: Slab<Val, val::Data>,
+}
+
+impl Func {
+    pub fn builder() -> Builder {
+        Builder::new()
+    }
+}
+
+pub struct Builder {
+    curr: bb::Partial,
+    bbs: Slab<BasicBlock, bb::Data>,
+    vals: Slab<Val, val::Data>,
+    names: HashMap<String, Val>,
+}
+
+impl Builder {
+    pub fn new() -> Builder {
+        Builder {
+            curr: bb::Partial::new(),
+            bbs: Slab::new(),
+            vals: Slab::new(),
+            names: HashMap::new(),
+        }
+    }
 }
