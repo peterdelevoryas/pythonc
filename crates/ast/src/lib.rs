14adfe3a8a7f42b2ae2dbe61309a2cee034a7222
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-10-23 09:38:19 -0600

    Added explicate of Add

--- crates/ast/src/lib.rs
+++ crates/ast/src/lib.rs
@@ -41,8 +41,6 @@ extern crate lalrpop_util;
 //pub mod parse;
 //pub use parse::parse_program;
 
-pub mod explicated;
-
 pub type ParseError = lalrpop_util::ParseError<usize, token::Token, token::Error>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -62,6 +60,14 @@ pub enum Expression {
     List(Vec<Expression>),
     Dict(Vec<(Expression, Expression)>),
     Is(Box<Expression>, Box<Expression>),
+
+    // Just for explication
+    GetTag(Box<Expression>),
+    InjectFrom(Type, Box<Expression>),
+    ProjectTo(Type, Box<Expression>),
+    Let(Decls, Box<Expression>),
+    Tmp(Tmp),
+    Call(String, Vec<Expression>),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -75,7 +81,18 @@ pub enum Statement {
     Print(Expression),
     Assign(Target, Expression),
     Expression(Expression),
+    If(Box<Expression>, Block, Block),
     Newline,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Block {
+    pub statements: Vec<Statement>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Decls {
+    pub decls: Vec<(Tmp, Expression)>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -86,4 +103,106 @@ pub struct Module {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Program {
     pub module: Module,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum Type {
+    Int,
+    Bool,
+    Big,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Tmp(usize);
+
+pub trait Explicate {
+    fn explicate<G>(self, tmp_gen: &mut G) -> Self
+    where
+        G: Iterator<Item=Tmp>;
+}
+
+impl Explicate for Block {
+    fn explicate<G>(self, tmp_gen: &mut G) -> Block
+    where
+        G: Iterator<Item=Tmp>,
+    {
+        Block {
+            statements: self.statements.into_iter()
+                .map(|st| st.explicate(tmp_gen))
+                .collect()
+        }
+    }
+}
+
+impl Explicate for Statement {
+    fn explicate<G>(self, tmp_gen: &mut G) -> Statement
+    where
+        G: Iterator<Item=Tmp>,
+    {
+        use self::Statement::*;
+        use self::Expression;
+        use self::Target::*;
+        match self {
+            Print(e) => {
+                let e = e.explicate(tmp_gen);
+                Statement::Expression(Expression::Call("print_any".into(), vec![e]))
+            }
+            Assign(Name(name), e) => {
+                let e = e.explicate(tmp_gen);
+                Statement::Assign(Name(name), e)
+            }
+            Assign(Subscript(box base, box elem), e) => {
+                let base = base.explicate(tmp_gen);
+                let elem = elem.explicate(tmp_gen);
+                let e = e.explicate(tmp_gen);
+                Statement::Expression(Expression::Call("set_subscript".into(), vec![base, elem, e]))
+            }
+            Expression(e) => Expression(e.explicate(tmp_gen)),
+            If(box e, then, els) => {
+                let e = e.explicate(tmp_gen);
+                let then = then.explicate(tmp_gen);
+                let els = els.explicate(tmp_gen);
+                If(box e, then, els)
+            }
+            Newline => Newline,
+        }
+    }
+}
+
+const BIG_TAG: i32 = 3;
+
+impl Explicate for Expression {
+    fn explicate<G>(self, tmp_gen: &mut G) -> Expression
+    where
+        G: Iterator<Item=Tmp>
+    {
+        use self::Expression::*;
+        use self::Target::*;
+        use self::Type::*;
+        match self {
+            Input => Call("input_int".into(), vec![]),
+            Add(box l, box r) => {
+                let l = l.explicate(tmp_gen);
+                let r = r.explicate(tmp_gen);
+                let l_tmp = tmp_gen.next().unwrap();
+                let r_tmp = tmp_gen.next().unwrap();
+                let decls = Decls {
+                    decls: vec![
+                        (l_tmp, l),
+                        (r_tmp, r),
+                    ],
+                };
+                Let(decls, 
+                    box If(
+                        box Is(box GetTag(box Tmp(l_tmp)), box DecimalI32(BIG_TAG)),
+                        box InjectFrom(Big, box Call("add".into(), vec![ProjectTo(Big, box Tmp(l_tmp)),
+                                                                        ProjectTo(Big, box Tmp(r_tmp))])),
+                        box InjectFrom(Int, box Add(box ProjectTo(Int, box Tmp(l_tmp)),
+                                                    box ProjectTo(Int, box Tmp(r_tmp))))
+                    )
+                )
+            }
+            other => other,
+        }
+    }
 }
