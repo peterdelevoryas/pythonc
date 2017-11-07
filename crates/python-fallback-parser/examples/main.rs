48cf25846cf272f3a7b56281191ff6ded30b0afc
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-11-07 14:24:50 -0700

    Removed old pyyc-rust-python stuff

--- crates/python-fallback-parser/examples/main.rs
+++
@@ -1,8 +0,0 @@
-extern crate python_fallback_parser as parser;
-
-use parser::*;
-
-fn main() {
-    let p = "1 + 2\ninput()\nprint 3 + input()\nx = input()\ny=x";
-    println!("{:?}", parse_program_fallback(p).unwrap());
-}
--- crates/trans/src/syntax.rs
+++
@@ -1,6 +0,0 @@
-use std::fmt;
-use ia_32;
-
-pub trait Syntax<T> {
-    fn syntax(&self, asm: &T, f: &mut fmt::Formatter) -> fmt::Result;
-}
