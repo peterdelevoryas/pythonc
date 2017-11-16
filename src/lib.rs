0d8771ece89f86136ee6a247bf2f1efe1b3b0060
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-11-15 18:30:54 -0700

    Fixed compiler errors

--- src/lib.rs
+++ src/lib.rs
@@ -119,8 +119,11 @@ impl Pythonc {
         let mut virtualizer = virtualize::Virtualizer::new(flattener.var_data);
         let virtualized = flattener.units.virtualize(&mut virtualizer);
         if stop_stage == Stage::VAsm {
+            /*
             let fmt = virtualize::Formatter::new(&virtualizer);
             return write_out(fmt, out_path);
+            */
+            unimplemented!()
         }
 
         Ok(())
