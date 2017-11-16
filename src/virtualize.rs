6f5704e7881fb133466635a32577452589488417
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-11-15 19:07:00 -0700

    Added initial setup for vasm

--- src/virtualize.rs
+++ src/virtualize.rs
@@ -5,7 +5,6 @@ use raise;
 use explicate::Var;
 
 use std::collections::HashMap;
-use std::convert;
 
 #[derive(Debug, Clone, Hash)]
 pub enum Instr {
@@ -52,6 +51,96 @@ pub struct StackSlot {
     ebp_offset: i32,
 }
 
-pub struct Builder {
-    
+pub struct Module {
+    funcs: HashMap<raise::Func, Func>,
 }
+
+impl Module {
+    pub fn from(f: flat::Flattener) -> Module {
+        let mut funcs = HashMap::new();
+
+        for (func, function) in f.units {
+            funcs.insert(func, Func::from(function));
+        }
+
+        Module { funcs }
+    }
+}
+
+pub struct Func {
+    block: Block,
+}
+
+impl Func {
+    pub fn from(f: flat::Function) -> Func {
+        Func {
+            block: Block {
+                instrs: vec![]
+            }
+        }
+    }
+
+    fn function(&mut self, function: flat::Function) {
+        for arg in function.args {
+            self.function_arg(arg);
+        }
+        for stmt in function.body {
+            self.stmt(stmt);
+        }
+    }
+
+    fn function_arg(&mut self, var: Var) {
+        // do nothing by default
+    }
+
+    fn stmt(&mut self, stmt: flat::Stmt) {
+        match stmt {
+            flat::Stmt::Def(var, expr) => {
+            }
+            flat::Stmt::Discard(expr) => {
+                self.expr(expr)
+            }
+            flat::Stmt::Return(var) => {
+
+            }
+            flat::Stmt::If(cond, then, else_) => {
+
+            }
+        }
+    }
+
+    fn expr(&mut self, expr: flat::Expr) {
+        match expr {
+            flat::Expr::UnaryOp(op, var) => {
+
+            }
+            flat::Expr::BinOp(op, left, right) => {
+            }
+            flat::Expr::CallFunc(f, args) => {
+
+            }
+            flat::Expr::RuntimeFunc(name, args) => {
+            }
+            flat::Expr::GetTag(var) => {
+
+            }
+            flat::Expr::ProjectTo(var, ty) => {
+
+            }
+            flat::Expr::InjectFrom(var, ty) => {
+
+            }
+            flat::Expr::Const(i) => {
+
+            }
+            flat::Expr::LoadFunctionPointer(func) => {
+
+            }
+            flat::Expr::Copy(Var) => {
+
+            }
+        }
+    }
+}
+
+
