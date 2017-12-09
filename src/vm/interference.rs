1e8d126cdb50daa0433dc46aead5821f46fed4bd
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-12-09 14:22:16 -0700

    Added interference::Builder

--- src/vm/interference.rs
+++ src/vm/interference.rs
@@ -3,12 +3,66 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use vm::Reg;
 use vm::Var;
+use vm::FuncData;
+use vm::Visit;
+use vm;
 
 pub struct Graph {
+    graph: UnGraphMap<Node, ()>,
+    unspillable: HashSet<Var>,
+    colors: HashMap<Var, Color>,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum Node {
     Forced(Reg),
     Var(Var),
+}
+
+pub type Color = Reg;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Saturation {
+    Spillable(usize),
+    Unspillable(usize),
+    Forced,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum DSaturResult {
+    Success,
+    Spill(Var),
+}
+
+impl Graph {
+    pub fn build(func: &FuncData) -> Graph {
+        let mut builder = Builder::new();
+
+        builder.visit_func(func);
+
+        Graph {
+            graph: builder.graph,
+            unspillable: builder.unspillable,
+            colors: builder.colors,
+        }
+    }
+}
+
+struct Builder {
+    graph: UnGraphMap<Node, ()>,
+    unspillable: HashSet<Var>,
+    colors: HashMap<Var, Color>,
+}
+
+impl Builder {
+    fn new() -> Self {
+        Self {
+            graph: UnGraphMap::new(),
+            unspillable: HashSet::new(),
+            colors: HashMap::new(),
+        }
+    }
+}
+
+impl Visit for Builder {
 }
