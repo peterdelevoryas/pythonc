4d301ff9bbad57d6efede15bcd3d6cfdec74e1d6
Peter Delevoryas <peter.delevoryas@colorado.edu>
2017-12-17 14:01:00 -0700

    Created solver types

--- src/ssa/solver.rs
+++ src/ssa/solver.rs
@@ -1,1 +1,40 @@
 use petgraph::graphmap::UnGraphMap;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use ssa::*;
+use reg::Reg;
+use stack::Slot;
+
+#[derive(Debug)]
+pub struct Graph {
+    graph: UnGraphMap<Node, ()>,
+    unspillable: HashSet<Value>,
+    colors: HashMap<Value, Color>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Node {
+    Reg(Reg),
+    Value(Value),
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Color {
+    Reg(Reg),
+    Stack(Slot),
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Saturation {
+    Spillable(usize),
+    Unspillable(usize),
+    Forced,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum DSaturResult {
+    Success,
+    Spill(Value),
+}
+
+
